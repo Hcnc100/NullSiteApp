0be635453a92b1b4327391ae766c68384ac43820
Death <ricardo.pajaro.bc2d@gmail.com>
2022-06-19 13:39:37 -0500

    add dependency

---
+++ app/src/main/java/com/nullpointer/nullsiteadmin/ui/screens/infoProfile/InfoProfile.kt
@@ -0,0 +1,12 @@
+package com.nullpointer.nullsiteadmin.ui.screens.infoProfile
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InfoProfile() {
+
+}
+@Composable
+private fun EditImgProfile() {
+
+}
