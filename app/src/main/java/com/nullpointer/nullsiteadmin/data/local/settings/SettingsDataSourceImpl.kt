9c405e948c794d1bc809ed3a72c52e8450507ef3
Death <ricardo.pajaro.bc2d@gmail.com>
2022-09-22 09:42:52 -0500

    adding provide DataStore for injection

--- app/src/main/java/com/nullpointer/nullsiteadmin/data/local/settings/SettingsDataSourceImpl.kt
+++ app/src/main/java/com/nullpointer/nullsiteadmin/data/local/settings/SettingsDataSourceImpl.kt
@@ -1,33 +1,28 @@
 package com.nullpointer.nullsiteadmin.data.local.settings
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.nullpointer.nullsiteadmin.models.UserAuth
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class SettingsDataSourceImpl(
-    private val context: Context
+    private val dataStore: DataStore<Preferences>
 ) : SettingsDataSource {
 
     companion object {
         private const val KEY_EMAIL_USER = "KEY_EMAIL_USER"
-        private const val NAME_SETTINGS = "ADMIN_SETTINGS"
         private const val KEY_TOKEN_MSG_USER = "KEY_TOKEN_MSG_USER"
         private const val KEY_ID_USER = "KEY_ID_USER"
     }
-
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = NAME_SETTINGS)
 
     private val keyIdUser = stringPreferencesKey(KEY_ID_USER)
     private val keyEmailUser = stringPreferencesKey(KEY_EMAIL_USER)
     private val keyTokenMsgUser = stringPreferencesKey(KEY_TOKEN_MSG_USER)
 
-    override fun getUserAuth() = context.dataStore.data.map { pref ->
+    override fun getUserAuth() = dataStore.data.map { pref ->
         UserAuth(
             id = pref[keyIdUser] ?: "",
             email = pref[keyEmailUser] ?: "",
@@ -38,7 +33,7 @@ class SettingsDataSourceImpl(
     override fun isAuthUser(): Flow<Boolean> = getUserAuth().map { it.id.isNotEmpty() }
 
     override suspend fun saveUserAuth(userAuth: UserAuth) {
-        context.dataStore.edit { pref ->
+        dataStore.edit { pref ->
             pref[keyIdUser] = userAuth.id
             pref[keyEmailUser] = userAuth.email
             pref[keyTokenMsgUser] = userAuth.tokenMsg
@@ -46,13 +41,13 @@ class SettingsDataSourceImpl(
     }
 
     override suspend fun updateTokenMsg(newToken: String) {
-        context.dataStore.edit { pref ->
+        dataStore.edit { pref ->
             pref[keyTokenMsgUser] = newToken
         }
     }
 
     override suspend fun clearData() {
-        context.dataStore.edit { pref ->
+        dataStore.edit { pref ->
             pref.clear()
         }
     }
--- app/src/main/java/com/nullpointer/nullsiteadmin/inject/OthersModule.kt
+++ app/src/main/java/com/nullpointer/nullsiteadmin/inject/OthersModule.kt
@@ -1,6 +1,12 @@
 package com.nullpointer.nullsiteadmin.inject
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.nullpointer.nullsiteadmin.data.local.settings.SettingsDataSource
 import com.nullpointer.nullsiteadmin.data.local.settings.SettingsDataSourceImpl
 import com.nullpointer.nullsiteadmin.domain.compress.CompressImgRepoImpl
@@ -11,14 +17,31 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+
 @Module
 @InstallIn(SingletonComponent::class)
 object OthersModule {
+
+    private const val NAME_SETTINGS = "ADMIN_SETTINGS"
+
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(
+        @ApplicationContext appContext: Context
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            produceFile = { appContext.preferencesDataStoreFile(NAME_SETTINGS) }
+        )
+    }
+
     @Provides
     @Singleton
     fun provideSettingsDataSource(
-        @ApplicationContext context: Context
-    ): SettingsDataSource = SettingsDataSourceImpl(context)
+        dataStore: DataStore<Preferences>
+    ): SettingsDataSource = SettingsDataSourceImpl(dataStore)
 
     @Singleton
     @Provides
