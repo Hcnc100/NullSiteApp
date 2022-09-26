9414737418d96a1b6d5729c511050fa493121ed4
Death <ricardo.pajaro.bc2d@gmail.com>
2022-09-26 12:49:59 -0500

    adding Crashlytics

--- app/src/main/java/com/nullpointer/nullsiteadmin/core/utils/ExceptionManager.kt
+++ app/src/main/java/com/nullpointer/nullsiteadmin/core/utils/ExceptionManager.kt
@@ -2,6 +2,8 @@ package com.nullpointer.nullsiteadmin.core.utils
 
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import com.nullpointer.nullsiteadmin.R
 import kotlinx.coroutines.channels.Channel
 import timber.log.Timber
@@ -9,6 +11,7 @@ import timber.log.Timber
 object ExceptionManager {
     const val NO_INTERNET_CONNECTION = "NO_INTERNET_NETWORK"
     const val SERVER_TIME_OUT = "SERVER_TIME_OUT"
+    private val crash = Firebase.crashlytics
 
     fun getMessageForException(throwable: Throwable, message: String): Int {
         return getMessageForException(Exception(throwable), message)
@@ -22,17 +25,23 @@ object ExceptionManager {
         }
     }
 
-    private fun getMessageForMsgException(message: String?): Int {
+    private fun getMessageForMsgException(message: String?, exception: Exception): Int {
         return when (message) {
             NO_INTERNET_CONNECTION -> R.string.error_network
             SERVER_TIME_OUT -> R.string.error_time_out
-            else -> R.string.error_unknown
+            else -> {
+                crash.recordException(exception)
+                R.string.error_unknown
+            }
         }
     }
 
     fun getMessageForException(exception: Exception, message: String): Int {
         Timber.e("${message}: $exception")
-        return getMessageForExceptionAuth(exception) ?: getMessageForMsgException(exception.message)
+        return getMessageForExceptionAuth(exception) ?: getMessageForMsgException(
+            exception.message,
+            exception = exception
+        )
     }
 
     fun sendMessageErrorToException(exception: Exception, message: String, channel: Channel<Int>) {
