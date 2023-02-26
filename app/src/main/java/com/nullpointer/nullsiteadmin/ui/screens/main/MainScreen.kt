724b9d1b697060b928118752984b13c4c0d30531
Death <ricardo.pajaro.bc2d@gmail.com>
2023-02-26 09:15:27 -0600

    remove DetailsTransition.kt

--- app/src/main/java/com/nullpointer/nullsiteadmin/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/nullpointer/nullsiteadmin/ui/screens/main/MainScreen.kt
@@ -1,33 +1,25 @@
 package com.nullpointer.nullsiteadmin.ui.screens.main
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.nullpointer.nullsiteadmin.presentation.AuthViewModel
 import com.nullpointer.nullsiteadmin.ui.interfaces.ActionRootDestinations
 import com.nullpointer.nullsiteadmin.ui.navigator.RootNavGraph
 import com.nullpointer.nullsiteadmin.ui.screens.NavGraphs
-import com.nullpointer.nullsiteadmin.ui.screens.animation.DetailsTransition
 import com.nullpointer.nullsiteadmin.ui.screens.destinations.InfoProfileDestination
 import com.nullpointer.nullsiteadmin.ui.screens.states.MainScreenState
 import com.nullpointer.nullsiteadmin.ui.screens.states.rememberMainScreenState
 import com.nullpointer.nullsiteadmin.ui.share.NavigatorDrawer
 import com.nullpointer.nullsiteadmin.ui.share.ToolbarMenu
 import com.ramcosta.composedestinations.DestinationsNavHost
-import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
-import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.dependency
+import com.ramcosta.composedestinations.rememberNavHostEngine
 
-@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
 @RootNavGraph
-@Destination(
-    style = DetailsTransition::class
-)
+@Destination
 @Composable
  fun MainScreen(
     authViewModel: AuthViewModel,
@@ -51,16 +43,11 @@ import com.ramcosta.composedestinations.navigation.dependency
         },
     ) { paddingValues ->
 
-        val navHostEngine = rememberAnimatedNavHostEngine(
-            navHostContentAlignment = Alignment.BottomEnd,
-            rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING,
-        )
-
         DestinationsNavHost(
             startRoute = InfoProfileDestination,
             navGraph = NavGraphs.home,
             navController = mainScreenState.navController,
-            engine = navHostEngine,
+            engine = rememberNavHostEngine(),
             modifier = Modifier.padding(paddingValues),
             dependenciesContainerBuilder = {
                 dependency(actionRootDestinations)
