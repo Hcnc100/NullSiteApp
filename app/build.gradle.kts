2b4c18227cc0dd7c6293a7951df511569183d4d2
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2026-09-20 21:56:13 -0600

    Bump app version to 4.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.nullpointer.nullsiteadmin"
         minSdk = 21
         targetSdk = 36
-        versionCode = 21
-        versionName = "4.0.2"
+        versionCode = 22
+        versionName = "4.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
